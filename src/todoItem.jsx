a3b749d2002aa35f1db28d488edb29875a1d05e3
brendon <200~108491991+BrendonTheDev@users.noreply.github.com>
2023-07-07 14:36:13 -0400

    Complete with local stor

---
+++ src/todoItem.jsx
@@ -0,0 +1,19 @@
+export function TodoItem({ completed, id, title, toggleTodo, deleteTodo }) {
+  return (
+    <li>
+      <label>
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={(e) => toggleTodo(id, e.target.checked)}
+          // check e on save need to delete parenthesis added by prettier
+          // use dprint or downgrade to 2017 prettier
+        />
+        {title}
+      </label>
+      <button onClick={() => deleteTodo(id)} className="btn btn-danger">
+        Delete
+      </button>
+    </li>
+  );
+}
